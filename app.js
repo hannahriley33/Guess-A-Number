7fa31ba29252d0ac72fb7fc10ba597de3d4b7d2b
Hannah Riley <riley.hannahm@gmail.com>
2020-01-15 09:59:45 -0800

    js working!!!!!!

--- app.js
+++ app.js
@@ -1,19 +1,56 @@
+import compareNumbers from "./compareNumbers.js";
+
 // Getting DOM Variables
 const button = document.getElementById("button");
 const image = document.getElementById("img");
 const wordsRules = document.getElementById("words");
 const attemptCount = document.getElementById("remainingGuesses");
 const resultUpdate = document.getElementById("result");
+const guessLowOrHigh = document.getElementById("highOrLow");
+const theirGuess = document.getElementById("whatTheyGuess");
 
 // Initialize State
 let tries = 4;
+
+let randomNumber = 5;
+//Math.floor(Math.random() * 10);
 
 // User Interaction
 button.addEventListener('click', () => {
    // State Change
     tries --;
-    resultUpdate.textContent = tries;
+    attemptCount.textContent = tries;
 
+   
+
+      
+    const compareResult = compareNumbers(Number(theirGuess.value), randomNumber);
+    console.log(compareResult);
+  
     // Update DOM
+    if (compareResult === 0) {
+        guessLowOrHigh.textContent = 'Correct!';
+        
+        image.src="https://www.modernman.com/wp-content/uploads/2017/09/635820385849834826-477057486_Nwc9fGvgEnzm6z71XMtCpAV3o1_500.png";
+   // document.getElementById('button').disabled = true;
+
+    };
+
+    if (compareResult === -1) {
+        guessLowOrHigh.textContent = 'Your guess is too low';
+    };
     
-})
+     if (compareResult === 1) {
+        guessLowOrHigh.textContent = 'Your guess is too high';
+    };
+
+    // if (tries === 0) {
+    //     return 'Guesses are up. You lose';
+    // }
+  
+
+ // create reduce count function 
+ // if count =0 , disable button
+    
+    
+});
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,21 @@
+import compareNumbers from "../compareNumbers.js";
+
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('checkng if compare numbers === 0', function(assert) {
     //Arrange
+    const expected = 0;
+    const userGuess = 5;
+    const correctNumber = 5;
     // Set up your parameters and expectations
 
     //Act 
     // Call the function you're testing and set the result to a const
-
+const result = compareNumbers(userGuess, correctNumber);
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(expected, result);
 });
