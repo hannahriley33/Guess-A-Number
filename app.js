71d79ed44488dccdebc5d89acbd601175f7b4435
Hannah Riley <riley.hannahm@gmail.com>
2020-01-14 16:55:10 -0800

    button & guesses remaining works

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-// DOM variables
+// Getting DOM Variables
 const button = document.getElementById("button");
-const
+const image = document.getElementById("img");
+const wordsRules = document.getElementById("words");
+const attemptCount = document.getElementById("remainingGuesses");
+const resultUpdate = document.getElementById("result");
+
+// Initialize State
+let tries = 4;
+
+// User Interaction
+button.addEventListener('click', () => {
+   // State Change
+    tries --;
+    resultUpdate.textContent = tries;
+
+    // Update DOM
+    
+})
